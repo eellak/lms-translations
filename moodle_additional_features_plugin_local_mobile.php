182e8b77f809d472d55f526636f7c82c8b204c10
dalexo <dalexopoulos@gmail.com>
2015-10-05 21:37:50 +0300

    translations of plugin
    recordingsbn & moodle mobile additional features

---
+++ moodle_additional_features_plugin_local_mobile.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'local_mobile', language 'en'.
+ *
+ * @package   local_mobile
+ * @copyright 2014 Juan Leyva <juan@moodle.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['cannotcreatetoken'] = 'Η αυτόματη δημιουργία κλειδιών δεν είναι διαθέσιμη στους διαχειριστές του ιστοτόπου (πρέπει να δημιουργήσουν ένα κλειδί χειροκίνητα στον ιστότοπο)';
+$string['local_mobiledescription'] = 'Πρόσθετο που επεκτείνει τις λειτουργικότητες των υπηρεσιών για κινητά';
+$string['pluginname'] = 'Επιπρόσθετες λειτουργικότητες Moodle για κινητά';
+$string['pluginnotenabledorconfigured'] = 'Το πρόσθετο Moodle Mobile πρέπει να είναι ενεργοποιημένο και παραμετροποιημένο για να επιτραπεί η έναρξη της εφαρμογής';
+$string['local_mobilesettings'] = 'Ρυθμίσεις';
+$string['local_mobiletypeoflogin'] = 'Επιλέξτε τον τρόπο ταυτοποίησης';
+$string['local_mobiletypeoflogin_key'] = 'Τρόπος ταυτοποίησης';
+$string['loginintheapp'] = 'Μέσω της εφαρμογής';
+$string['logininthebrowser'] = 'Μέσω του παραθύρου του περιηγητή (για πρόσθετα SSO)';
---
+++ recordingsbn.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Language File
+ *
+ * @package   mod_bigbluebutton
+ * @author    Jesus Federico  (jesus [at] blindsidenetworks [dt] com)
+ * @copyright 2011-2014 Blindside Networks Inc.
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v2 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'RecordingsBN';
+$string['modulenameplural'] = 'RecordingsBN';
+$string['modulename_help'] = 'Χρησιμοποιήστε το πρόσθετο RecordingsBN σαν μέσο για την πρόσβαση στις εγγραφές BigBlueButton που σχετίζονται με αυτό το μάθημα.';
+$string['modulename_link'] = 'RecordingsBN/view';
+$string['recordingsbnname'] = 'Όνομα Εγγραφών';
+$string['recordingsbnname_help'] = 'Το RecordingsBN προβάλλει μια λίστα αναπαραγωγής εγγραφών σε έναν διακομιστή BigBlueButton και δίνει άμεση πρόσβαση σε αυτή.';
+$string['recordingsbn'] = 'RecordingsBN';
+$string['pluginadministration'] = 'Διαχείριση recordingsbn';
+$string['pluginname'] = 'RecordingsBN';
+$string['recordingsbn:addinstance'] = 'Προσθήκη νέου πόρου με αναπαραγωγή εγγραφών';
+$string['recordingsbn:view'] = 'Δείτε τις εγγραφές';
+
+$string['mod_form_field_ui_html'] = 'Δείξε τον πίνακα σε μορφή html';
+
+$string['config_general'] = 'Γενικές ρυθμίσεις';
+$string['config_general_description'] = 'Αυτές οι ρυθμίσεις πρέπει <b>πάντα</b> να χρησιμοποιούνται';
+$string['config_feature_ui_html_default'] = 'Το UI σε μορφή html ενεργοποιείται από προεπιλογή';
+$string['config_feature_ui_html_default_description'] = 'Αν ενεργοποιηθεί ο πίνακας των εγγραφών φαίνεται σε μορφή HTML από προεπιλογή.';
+$string['config_feature_ui_html_editable'] = 'Το UI σαν html μπορεί να επεξεργαστεί';
+$string['config_feature_ui_html_editable_description'] = 'Το UI σαν html μπορεί να επεξεργαστεί όταν το recordingbn προστίθεται ή ενημερώνεται.';
+
+$string['view_noguests'] = 'Το πρόσθετο RecordingsBN δεν είναι διαθέσιμο στους επισκέπτες';
+$string['view_delete_confirmation'] = 'Είστε σίγουρη/ος για την διαγραφή της εγγραφής?';
+$string['view_delete_confirmation'] = 'Είστε σίγουρη/ος για την διαγραφή της εγγραφής?';
+$string['view_dependency_error'] = 'Πρέπει να έχετε εγκατεστημένο το πρόσθετο BigBlueButtonBN Activity';
+$string['view_head_actionbar'] = 'Toolbar';
+$string['view_head_activity'] = 'Ενέργιεα';
+$string['view_head_course'] = 'Μάθημα';
+$string['view_head_date'] = 'Ημερομηνία';
+$string['view_head_description'] = 'Περιγραφή';
+$string['view_head_length'] = 'Μήκος';
+$string['view_head_duration'] = 'Διάρκεια';
+$string['view_head_recording'] = 'Εγγραφή';
+$string['view_duration_min'] = 'λεπτά';
+$string['view_no_recordings'] = 'Δεν υπάρχουν εγγραφές για εμφάνιση';
+
+$string['event_resource_page_viewed'] = 'Η σελίδα RecordingsBN προβλήθηκε';
+$string['event_recording_published'] = 'Η εγγραφή δημοσιεύτηκε';
+$string['event_recording_unpublished'] = 'Η εγγραφή αποσύρθηκε';
+$string['event_recording_deleted'] = 'H εγγραφή διεγράφη';
